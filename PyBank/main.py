7cac055287a3c839402c40da9ae44df06c9d99d8
DarrylHewitt <d.hewitt7845@gmail.com>
2023-08-26 19:37:02 +0100

    Equalised and zipped lists and calculated min/max

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,11 +13,11 @@ with open(csvpath) as csvfile: # Open and read the csv file
     months = []
     total_profit_loss = 0
     
-    for i in budget_data:
-        months.append(i[0])
-        profit_loss = int(i[1])
+    for month in budget_data:
+        months.append(month[0])
+        profit_loss = int(month[1])
         total_profit_loss += profit_loss 
-        
+
     print("Total Months:", len(months))
     print("Total:", total_profit_loss)
 
@@ -34,15 +34,44 @@ with open(csvpath) as csvfile: # Open and read the csv file
         net_change = int(j[1]) - prev_val # Subtract the current cell value from the previous value
         prev_val = int(j[1]) # Once calculated, update the previous value to the current cell value
         net_change_list.append(net_change) # Add the calculation to the list
-
+        
     avg_net_change = sum(net_change_list) / len(net_change_list) #Find the average monthly change
-    
+
     print("Average change:", avg_net_change)
 
+    # Make the two lists the same size by adding a None value to the beginning of the shorter list
+    max_length = max(len(months), len(net_change_list))
+    months = [None] * (max_length - len(months)) + months 
+    net_change_list = [0] * (max_length - len(net_change_list)) + net_change_list
 
+    combined_list = list(zip(months, net_change_list)) # Zip the two lists together
+    for month in combined_list:
+        print(month)
 
-            
-    
+    max_inc = 0
+    max_dec = 0
+    max_mon = None
+    min_mon = None
+   
+    for month, profit_loss in combined_list:
+        if profit_loss > max_inc:
+            max_inc = profit_loss
+            max_month = month
+        else:
+            profit_loss = profit_loss + 1
+
+    print("Greatest Increase in Profits:", max_month, max_inc)
+
+    for month, profit_loss in combined_list:
+        if profit_loss < max_dec:
+            max_dec = profit_loss
+            min_mon = month
+
+    print("Greatest Decrease in Profits:", min_mon, max_dec)        
+        
+
+        
+
     
 
 
