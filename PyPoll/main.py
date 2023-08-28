81af61c0b83b216dc88880caf25a9291f78b2911
DarrylHewitt <d.hewitt7845@gmail.com>
2023-08-28 19:30:15 +0100

    Completed and commented code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,21 +3,60 @@ import csv
 
 csvpath = os.path.join("Resources", "election_data.csv")
 
+candidate_data = {}
+
 with open(csvpath) as csvfile:
 
     election_data = csv.reader(csvfile, delimiter=',')
 
     next(election_data)
 
+    print("------------------")
+
     print("Election Results")
 
     print("------------------")
 
-    ballot_count = []
+# The total number of votes cast:
 
-    for ballot in election_data:
-         ballot_count.append(ballot[0])
+    ballot_count = [] # Declare a ballot count list
 
-    print(f"Total votes:", (len(ballot_count)))
+    for row in election_data:
+         ballot_count.append(row[0]) # Add each ballot count to the list
+
+    total_votes = len(ballot_count) # Assign the length of the list to a variable to use later
+         
+    print(f"Total votes:", total_votes)
 
     print("------------------")
+
+    csvfile.seek(0) # Reset the file pointer
+    next(election_data) # Skip the header row again
+
+    for row in election_data:
+
+        candidate_name = row[2] # Declare a candidate name variable to the candidate column
+
+        if candidate_name in candidate_data: # Check if the candidate is in the dictionary
+            candidate_data[candidate_name] += 1 # If they are, then increase their vote count by one
+        else:
+            candidate_data[candidate_name] = 1 # If they aren't, then start them off with a vote count of one
+
+    for candidate, votes in candidate_data.items(): # Loop through the key value pairs
+        percentage = (votes / total_votes) * 100 # Calculate percentage 
+        percentage_rounded = round(percentage, 3) # Round the percentage to the required decimal places
+        print(f"{candidate}: {percentage_rounded}% ({votes})")
+
+    print("------------------")
+    
+    max_votes = max(candidate_data.values()) # Declare a variable for max votes and perform the max function. We only want to calculate the vote count, so we use .values()
+    for candidate, votes in candidate_data.items(): # We use .items() to loop through the values and their keys
+        if votes == max_votes: # If the total number of votes for a candidate matches the calculated max vote then ...
+            print(f"Winner: {candidate}") # Declare the winner
+
+    print("------------------")
+
+
+        
+
+
